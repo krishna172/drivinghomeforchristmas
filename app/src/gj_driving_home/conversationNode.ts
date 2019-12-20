c96e418b7e3fc0bdf236636aa94f40ebd9cb2c78
lukas <l.knoch@gmx.at>
2019-12-20 22:27:48 +0100

    added ctor

--- app/src/gj_driving_home/conversationNode.ts
+++ app/src/gj_driving_home/conversationNode.ts
@@ -3,6 +3,13 @@ import {Emotion} from "./emotion";
 export class ConversationNode {
     id : string;
     text : string;
-    options: { [id: number] : ConversationNode; };
+    options: Map<Emotion, ConversationNode>;
+
+
+    constructor(id: string, text: string, options: Map<Emotion, ConversationNode>) {
+        this.id = id;
+        this.text = text;
+        this.options = options;
+    }
 }
 
