20ba2bde62956f58f6bdc77db1a39735c0f8bf90
VeitFrick <veit.frick@aau.at>
2019-12-21 21:51:59 +0100

    lights

--- app/src/gj_driving_home/scenes/steeringwheelScene.ts
+++ app/src/gj_driving_home/scenes/steeringwheelScene.ts
@@ -6,14 +6,16 @@ import {ConversationNode} from "../conversationNode";
 import LoaderPlugin = Phaser.Loader.LoaderPlugin;
 import {SceneLoadingData} from "./sceneLoadingData";
 import CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
+import Light = Phaser.GameObjects.Light;
 
 export class SteeringWheelScene extends Phaser.Scene {
     private dbox: DialogBox;
     private _sceneData: SceneLoadingData;
     private cursor: CursorKeys;
     private steeringWheel: Phaser.GameObjects.Sprite;
+    private background: Phaser.GameObjects.Image;
     private cactuses: Array<Phaser.GameObjects.Sprite>;
-
+    private lightsList: Array<Phaser.GameObjects.Light>;
     readonly fpsInterpolation = 59; // how many interpolation point
     readonly fpsSkip = 1; // after how many frames show the next interp step
 
@@ -51,21 +53,34 @@ export class SteeringWheelScene extends Phaser.Scene {
     preload(): void {
 
         this.load.json('scene', './assets/sceneDescriptions/' + this._sceneData.key + '.json');
-        this.load.image(AssetGlobals.BG_ROAD, "./assets/backgrounds/" + AssetGlobals.BG_ROAD + ".jpg");
-        this.load.image(AssetGlobals.SPRITE_STEERING, "./assets/sprites/" + AssetGlobals.SPRITE_STEERING + ".png");
-        this.load.image(AssetGlobals.SPRITE_CACTUS, "./assets/sprites/" + AssetGlobals.STREET_LAMP + ".png");
+        this.load.image(AssetGlobals.BG_ROAD, ["./assets/backgrounds/" + AssetGlobals.BG_ROAD + ".jpg","./assets/lm.png"]);
+        this.load.image(AssetGlobals.SPRITE_STEERING, ["./assets/sprites/" + AssetGlobals.SPRITE_STEERING + ".png","./assets/lm.png"]);
+        this.load.image(AssetGlobals.SPRITE_CACTUS, ["./assets/sprites/" + AssetGlobals.STREET_LAMP + ".png","./assets/lm.png"]);
+
+
+
     }
 
     create(): void {
-        this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, AssetGlobals.BG_ROAD);
+        this.background  =this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, AssetGlobals.BG_ROAD);
+        this.background.setScale(1.2,1.2);
+        this.background.setPipeline('Light2D');
+
         this.steeringWheel = this.add.sprite(this.game.renderer.width * 0.5, this.game.renderer.height * 0.75, AssetGlobals.SPRITE_STEERING);
-        this.steeringWheel.setScale(0.4, 0.4);
-        this.cameras.main.setZoom(1.2);
-        this.cameras.main.centerOn(this.game.renderer.width / 2, this.game.renderer.height / 2);
+        this.steeringWheel.setPipeline('Light2D');
+//this.steeringWheel.setScale(0.4, 0.4);
+        //this.cameras.main.setZoom(1.2);
+        //this.cameras.main.centerOn(this.game.renderer.width / 2, this.game.renderer.height / 2);
         this.cactuses = [
             this.add.sprite(0, 0, AssetGlobals.SPRITE_CACTUS),
+            //this.add.sprite(0, 0, AssetGlobals.SPRITE_CACTUS),
             this.add.sprite(0, 0, AssetGlobals.SPRITE_CACTUS),
             this.add.sprite(0, 0, AssetGlobals.SPRITE_CACTUS),
+        ];
+        this.lightsList = [
+            this.lights.addLight(0, 0, 0),
+            this.lights.addLight(0, 0, 200),
+            this.lights.addLight(0, 0, 200),
         ];
         this.cameras.main.x -= this.INIT_OFFSET;
         this.cursor = this.input.keyboard.createCursorKeys();
@@ -73,20 +88,26 @@ export class SteeringWheelScene extends Phaser.Scene {
         this.initCactusPosition();
         let game = this;
         this.renderActionText("Debug for steering! Press left and right to steer")
-
+        for (var cactus of this.cactuses) {
+            cactus.setPipeline('Light2D');
+        }
+        //var light  = this.lights.addLight(500, 250, 200);
+        this.lights.enable().setAmbientColor(0x555555);
+        this.cactuses[0].alpha=100;
     }
 
     initCactusPosition() {
-        this.cactuses[0].setOrigin(0, 1);
+        //this.cactuses[0].setOrigin(0, 1);
         this.cactuses[1].setOrigin(0, 1);
         this.cactuses[2].setOrigin(0, 1);
 
+
         // compute interpolation table
         var startX = 800;
-        var startY = 700;
+        var startY = 600;
         var startZ = 0.01;
         var endX = 100;
-        var endY = 750;
+        var endY = 630;
         var endZ = 0.2;
 
         var i = 0;
@@ -107,18 +128,20 @@ export class SteeringWheelScene extends Phaser.Scene {
         //console.log(this.interpolationZ);
 
 
-        this.cactuses[0].setScale(this.interpolationZ[this.initFps0], this.interpolationZ[this.initFps0]);
+        //this.cactuses[0].setScale(this.interpolationZ[this.initFps0], this.interpolationZ[this.initFps0]);
         this.cactuses[1].setScale(this.interpolationZ[this.initFps1], this.interpolationZ[this.initFps1]);
         this.cactuses[2].setScale(this.interpolationZ[this.initFps2], this.interpolationZ[this.initFps2]);
 
-        this.cactuses[0].x = this.interpolationX[this.initFps0];
-        this.cactuses[0].y = this.interpolationY[this.initFps0];
+        //this.cactuses[0].x = this.interpolationX[this.initFps0];
+        //this.cactuses[0].y = this.interpolationY[this.initFps0];
 
         this.cactuses[1].x = this.interpolationX[this.initFps1];
         this.cactuses[1].y = this.interpolationY[this.initFps1];
 
         this.cactuses[2].x = this.interpolationX[this.initFps2];
         this.cactuses[2].y = this.interpolationY[this.initFps2];
+        this.updateLights();
+
 
     }
 
@@ -127,13 +150,24 @@ export class SteeringWheelScene extends Phaser.Scene {
         this.currentFps++;
         if(this.currentFps % this.fpsSkip == 0)
             this.linearInterpolation();
+        this.updateLights();
+
+    }
+
+    private updateLights() {
+        //this.lightsList[0].x = this.cactuses[0].x;
+        //this.lightsList[0].y = this.cactuses[0].y-10;
+        this.lightsList[1].x = this.cactuses[1].x;
+        this.lightsList[1].y = this.cactuses[1].y-10;
+        this.lightsList[2].x = this.cactuses[2].x;
+        this.lightsList[2].y = this.cactuses[2].y-10;
     }
 
     linearInterpolation() {
-        this.initFps0 = (this.initFps0+1) % this.fpsInterpolation;
-        this.cactuses[0].x = this.interpolationX[this.initFps0];
-        this.cactuses[0].y = this.interpolationY[this.initFps0];
-        this.cactuses[0].setScale(this.interpolationZ[this.initFps0], this.interpolationZ[this.initFps0]);
+        //this.initFps0 = (this.initFps0+1) % this.fpsInterpolation;
+        //this.cactuses[0].x = this.interpolationX[this.initFps0];
+        //this.cactuses[0].y = this.interpolationY[this.initFps0];
+        //this.cactuses[0].setScale(this.interpolationZ[this.initFps0], this.interpolationZ[this.initFps0]);
 
         this.initFps1 = (this.initFps1+1) % this.fpsInterpolation;
         // other side of the street
@@ -145,7 +179,6 @@ export class SteeringWheelScene extends Phaser.Scene {
         this.cactuses[2].x = this.interpolationX[this.initFps2];
         this.cactuses[2].y = this.interpolationY[this.initFps2];
         this.cactuses[2].setScale(this.interpolationZ[this.initFps2], this.interpolationZ[this.initFps2]);
-
     }
 
     updateSteeringWheelAndCamera() {
@@ -153,12 +186,13 @@ export class SteeringWheelScene extends Phaser.Scene {
         var limit = false;
         //left is pressed and we are in the bounds
         if (this.cursor.left.isDown) {
-            if (camera.x < this.MAX_ANGLE - this.INIT_OFFSET) {
-                this.cameras.main.x += this.STEP;
+            if (camera.scrollX < this.MAX_ANGLE - this.INIT_OFFSET) {
+                this.cameras.main.scrollX += this.STEP;
                 this.steeringWheel.x -= this.STEP;
                 // enable animation steering wheel if it ain't turned too much
                 if (this.steeringWheel.angle > this.MIN_ANGLE) {
                     this.steeringWheel.angle -= this.STEP;
+
                 }
             } else {
                 // we reach the limit
@@ -167,8 +201,8 @@ export class SteeringWheelScene extends Phaser.Scene {
         } else {
             //right is pressed and we are in the bounds
             if (this.cursor.right.isDown) {
-                if (camera.x > this.MIN_ANGLE - this.INIT_OFFSET) {
-                    this.cameras.main.x -= this.STEP;
+                if (camera.scrollX > this.MIN_ANGLE - this.INIT_OFFSET) {
+                    this.cameras.main.scrollX-= this.STEP;
                     this.steeringWheel.x += this.STEP;
                     if (this.steeringWheel.angle < this.MAX_ANGLE) {
                         this.steeringWheel.angle += this.STEP;
